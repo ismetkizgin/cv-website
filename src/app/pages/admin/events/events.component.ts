725b007e9c1d5defae3f63b5a6dcb4fc7574c989
frknsgt <furkan.v.sk@gmail.com>
2020-12-13 23:38:38 +0300

    refactor: event page design revised

--- src/app/pages/admin/events/events.component.ts
+++ src/app/pages/admin/events/events.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AddEventComponent, DialogWindowComponent } from 'src/app/components';
+import { Event } from '../../../models';
 
 @Component({
   selector: 'app-events',
@@ -18,18 +19,18 @@ export class EventsComponent implements OnInit {
     private _translateService: TranslateService,
     private _snackBar: MatSnackBar,
   ) { }
-  
+
   searchText = "";
-  events:Array<Event>;
+  events: Array<Event>;
   async ngOnInit() {
-    try{
-      this.events=<Array<Event>> await this._eventService.listAsync();
-    }catch(error){
+    try {
+      this.events = <Array<Event>>await this._eventService.listAsync();
+    } catch (error) {
       this._eventService.errorNotification(error);
     }
   }
 
-  openAddEventModal(Id=null){
+  openAddEventModal(Id = null) {
     const diologRef = this._dialog.open(AddEventComponent, {
       width: '500px',
       data: this.events.find(
@@ -39,40 +40,40 @@ export class EventsComponent implements OnInit {
     diologRef.afterClosed().subscribe((result: any) => {
       if (result) this.ngOnInit();
     });
-}
+  }
 
-async eventDelete(Id) {
-  const diologRef = this._dialog.open(DialogWindowComponent, {
-    data: {
-      message: 'Are you sure you want to delete the user ?',
-      icon: 'fa fa-exclamation',
-    },
-  });
+  async eventDelete(Id) {
+    const diologRef = this._dialog.open(DialogWindowComponent, {
+      data: {
+        message: 'Are you sure you want to delete the user ?',
+        icon: 'fa fa-exclamation',
+      },
+    });
 
-  diologRef.afterClosed().subscribe(async (result: boolean) => {
-    if (result) {
-      try {
-        await this._eventService.deleteAsync({ Id });
-        this.events.splice(
-          this.events.findIndex((event) => event.Id == Id),
-          1
-        );
-        let notificationMessage: string;
-        this._translateService
-          .get('Event information was successfully deleted')
-          .subscribe((value) => (notificationMessage = value));
+    diologRef.afterClosed().subscribe(async (result: boolean) => {
+      if (result) {
+        try {
+          await this._eventService.deleteAsync({ Id });
+          this.events.splice(
+            this.events.findIndex((event) => event.Id == Id),
+            1
+          );
+          let notificationMessage: string;
+          this._translateService
+            .get('Event information was successfully deleted')
+            .subscribe((value) => (notificationMessage = value));
 
-        this._snackBar.open(notificationMessage, 'X', {
-          duration: 3000,
-          panelClass: 'notification__success',
-          verticalPosition: 'bottom',
-          horizontalPosition: 'right',
-        });
-      } catch (error) {
-        this._eventService.errorNotification(error);
+          this._snackBar.open(notificationMessage, 'X', {
+            duration: 3000,
+            panelClass: 'notification__success',
+            verticalPosition: 'bottom',
+            horizontalPosition: 'right',
+          });
+        } catch (error) {
+          this._eventService.errorNotification(error);
+        }
       }
-    }
-  });
-}
+    });
+  }
 
 }
